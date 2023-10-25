c0f061f5513511e7e5cf890a572c29a979919af0
Vanessa Vargas <vanessa.vargas4f@gmail.com>
2023-10-25 18:22:37 -0300

    Implementando posts

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,17 @@
+const Post = ({ post }) => {
+    return (
+        <div className="post">
+            <img
+                className="cover"
+                src={`../../../public/assets/posts/${post.id}/capa.png`}
+                alt="Imagem de capa do post"
+            />
+
+            <h2 className="post-title">{post.titulo}</h2>
+
+            <button className="btn-read">Ler</button>
+        </div>
+    )
+}
+
+export default Post;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import Banner from "../../components/Banner/Banner";
+import Post from "../../components/Post";
+import posts from '../../json/posts.json';
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <ul className="posts">
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     </>
   );
 };
